4b904495b2e766056ef126b37626144714c685a5
Eduard <eduardvelez066@gmail.com>
2025-10-05 22:48:17 -0500

    serializers.py creado

---
+++ backend/apps/usuarios/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+Usuario = get_user_model()
+
+
+class RegistroSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = Usuario
+        fields = [
+            'username', 
+            'email', 
+            'first_name', 
+            'last_name', 
+            'telefono', 
+            'rol', 
+            'password', 
+            'password2'
+        ]
+        extra_kwargs = {
+            'email': {'required': True},
+            'first_name': {'required': False},
+            'last_name': {'required': False},
+        }
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({
+                "password": "Las contraseñas no coinciden."
+            })
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = Usuario.objects.create_user(**validated_data)
+        return user
+
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'first_name', 
+            'last_name', 
+            'telefono', 
+            'rol',
+            'date_joined'
+        ]
+        read_only_fields = ['id', 'date_joined']
