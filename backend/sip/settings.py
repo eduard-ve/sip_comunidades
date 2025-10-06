9b264214baf038ae0da8c495c5a19a4b8da0ee2f
Eduard <eduardvelez066@gmail.com>
2025-10-05 19:09:09 -0500

    Agregar dependencias y configuracion para utilizar JWT

--- backend/sip/settings.py
+++ backend/sip/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #DRF
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    #apps
+    'apps.usuarios',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +130,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configuracion de django rest framework
+RETS_FRAMEWORK = {
+    'DEFAULT:AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+#configuracion de simple jwt
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
+}
