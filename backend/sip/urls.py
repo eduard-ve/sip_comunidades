bba2aece443a8ecab440b916fd0ae49404e14bff
Eduard <eduardvelez066@gmail.com>
2025-10-15 13:11:46 -0500

    Ajustar la nueva ruta con su importacion correspondiente admin-superusuario

--- backend/sip/urls.py
+++ backend/sip/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import admin
 from django.urls import path, include
 
 # Rutas de la aplicación de usuarios
 urlpatterns = [
-    path('admin/', include('admin.site.urls')),
+    path('admin/', admin.site.urls),
     path('api/usuarios/', include('apps.usuarios.urls'))   
 ]
