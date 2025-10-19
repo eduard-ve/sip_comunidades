adc685bfe517e3e7ac983893dc8cb7b11739c718
Eduard <eduardvelez066@gmail.com>
2025-10-19 11:30:03 -0500

    Modelo relaciones familiares

---
+++ backend/apps/poblacion/models/relaciones.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .personas import Persona
+from .catalogos import TipoRelacion
+
+# Modelo de Relaciones Familiares entre Personas
+class RelacionFamiliar(models.Model):
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='relaciones')
+    familiar = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='familiares')
+    tipo_relacion = models.ForeignKey(TipoRelacion, on_delete=models.PROTECT)
+
+    def _str_(self):
+        return f"{self.persona} - {self.tipo_relacion} - {self.familiar}"
+    
+    class Meta:
+        verbose_name = "Relación Familiar"
+        verbose_name_plural = "Relaciones Familiares"
+        constraints = [
+            models.CheckConstraint(check=~models.Q(persona=models.F('familiar')), name='no_auto_relacion')
+ ]
