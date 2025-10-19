4783ae7059523715a1612b7bb542a9556e29fbf8
Eduard <eduardvelez066@gmail.com>
2025-10-19 12:36:15 -0500

    admin completado, modulos poblacion listo para aplicar las migraciones y realizar los respectivos tests verificando que si esta correcto la implementacion

--- backend/apps/poblacion/admin.py
+++ backend/apps/poblacion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from poblacion.models.catalogos import *
+from poblacion.models.personas import Persona
+from poblacion.models.relaciones import RelacionFamiliar
+
 
 # Register your models here.
+admin.site.register(TipoIdentificacion, NivelEducatico, Ocupacion, GrupoFamiliar,
+            EstadoCivil, Lengua, TipoRelacion, Persona, RelacionFamiliar)
