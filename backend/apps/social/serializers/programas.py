8904ce53aff464c68beb19187eeead212b401eb6
Eduard <eduardvelez066@gmail.com>
2025-10-29 07:39:35 -0500

    Serializer programas.py agregado, verificar funcionamiento respectivo

---
+++ backend/apps/social/serializers/programas.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from ..models import EstadoPrograma, ProgramaSocial
+
+class EstadoProgramaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EstadoPrograma
+        fields = ['id', 'nombre']
+
+class ProgramaSocialSerializer(serializers.ModelSerializer):
+    estado = EstadoProgramaSerializer(read_only=True)
+    estado_id = serializers.PrimaryKeyRelatedField(
+        queryset=EstadoPrograma.objects.all(), source='estado', writwe_only=True
+    )
+
+    class Meta:
+        model = ProgramaSocial
+        fields = ['id', 'nombre', 'descripcion', 'estado', 'estado_id', 'fecha_inicio', 'fecha_fin', 'responsable']
