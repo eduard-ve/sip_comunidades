857edcf3a69764b34db8657a56886b8ac9a7b29f
Eduard <eduardvelez066@gmail.com>
2025-10-30 16:41:13 -0500

    refactor(social): mejora la estructura del enrutamiento y simplifica las rutas

--- backend/apps/social/urls.py
+++ backend/apps/social/urls.py
@@ -1,17 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views.programas import EstadoProgramaViewSet, ProgramaSocialViewSet
 from .views.beneficiarios import ProgramaBeneficiarioViewSet, ActividadSocialViewSet, CoberturaProgramaViewSet
 
 # Configuración del router para las vistas del módulo social
 router = DefaultRouter()
-router.register(r'estados', EstadoProgramaViewSet, basename='estado')
-router.register(r'programas', ProgramaSocialViewSet, basename='programa')
-router.register(r'beneficiarios', ProgramaBeneficiarioViewSet, basename='beneficiario')
-router.register(r'actividades', ActividadSocialViewSet, basename='actividad')
-router.register(r'coberturas', CoberturaProgramaViewSet, basename='cobertura')
+router.register(r'estados', EstadoProgramaViewSet, basename='estados')
+router.register(r'programas', ProgramaSocialViewSet, basename='programas')
+router.register(r'beneficiarios', ProgramaBeneficiarioViewSet, basename='beneficiarios')
+router.register(r'actividades', ActividadSocialViewSet, basename='actividades')
+router.register(r'coberturas', CoberturaProgramaViewSet, basename='coberturas')
 
 # Definición de las URLs del módulo social
-urlpatterns = [
-    path('api/social/', include(router.urls))
-]
+urlpatterns = router.urls
